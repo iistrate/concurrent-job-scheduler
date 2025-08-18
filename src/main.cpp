b90455a86c2e46ce107226112625e5c0675a04cd
Ioan <iistrate3@gatech.edu>
2025-08-18 10:13:13 -0400

    adds basic scheduler usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "scheduler.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Job scheduler starting..." << std::endl;
+
+    const int N = (argc > 1) ? std::stoi(argv[1]) : 8;
+    
+    Scheduler s;
+    s.start(N);
+
+    std::vector<std::unique_ptr<Task>> tasks;
+    
+    for(auto& task: tasks) {
+        s.add(std::move(task));
+    }
+
+    s.stop();
+    s.join();
     return 0;
 }
 
