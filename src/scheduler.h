7d1a46be93549c064444f9be3ee52a051f12deb3
Ioan <iistrate3@gatech.edu>
2025-08-19 18:13:52 -0400

    exposes start, stop, cancel, join

--- src/scheduler.h
+++ src/scheduler.h
@@ -29,14 +29,13 @@ class Scheduler {
     std::priority_queue<Task_p, std::vector<Task_p>, cmp> pq;
     std::unordered_set<int> cancelled;
 
-    void cancel(int tid);
-    void start(const int N);
-    void stop();
-    void join();
-
 public:
     status_t add(Task_p t);
     void run();
+    void cancel(int tid);
+    void start(const int N);
+    void stop();
+    void join();    
 };
 
 
